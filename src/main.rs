05c06cf73a365296f9c7255b65359af1e3025085
aeyis <sungwoo@jopeconnect.com>
2022-04-30 16:00:00 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+    // gamestate = init game state
+    // input system
+    // ai system
+    // physics system
+    // render system
+    // audio system
+}
